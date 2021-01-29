0ebb851e2aeae57684d3ca6dd86ef9a6880010f8
Administrator <1746678613@qq.com>
2021-01-29 10:00:49 +0800

    ban1 初始化

---
+++ initshiro/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author: create by zhl
+ * @version: v1.0
+ * @description: PACKAGE_NAME
+ * @date:2021/1/29
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("init-bran1");
+    }
+}
